16d9aa741e09a24f7264dbf80222195a593c9a9f
Balta Ramírez <124750256+BaltaRT25@users.noreply.github.com>
2023-11-28 17:57:49 -0600

    Using setup
    Setup and setup_all added to calculator_test file for generating data that can be accessed through all tests.

--- Elixir roadmap/mix/roadmap/test/calculator_test.exs
+++ Elixir roadmap/mix/roadmap/test/calculator_test.exs
@@ -1,8 +1,18 @@
 defmodule CalculatorTest do
   use ExUnit.Case
 
-  test "add two numbers" do
-    assert Calculator.add(2, 2) == {:ok, 4}
+  # se va a inicializar 1 vez por módulo, va a ser global para todo el módulo
+  setup_all do
+    {:ok, number1: 2, number2: 2, number3: 0}
+  end
+
+  # se va a inicializar cada vez que se ejecuta un test
+  # setup do
+  #   {:ok, number1: 2, number2: 2, number3: 0}
+  # end
+
+  test "add two numbers", numbers do
+    assert Calculator.add(numbers[:number1], numbers[:number2]) == {:ok, 4}
   end
 
   test "Passing parameter as string instead of numbers" do
